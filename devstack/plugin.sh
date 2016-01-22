8c475b94531788e7a1fb94dc0fc8f76979705b77
Qeas <alexey.khodos@nexenta.com>
2016-01-22 18:17:19 +0300

    remove cinder folder if it exists before cloning

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,6 +2,7 @@
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     cd /home/ubuntu
+    rm -rf cinder
     git clone https://github.com/Nexenta/cinder.git
     cp -r cinder/cinder/volume/drivers/nexenta /opt/stack/cinder/cinder/volume/drivers
     cp cinder/cinder/exception.py /opt/stack/cinder/cinder/
