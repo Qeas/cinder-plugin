5bc73eaf598200ae5abc3da2374960ec3d0d92ef
Qeas <alexey.khodos@nexenta.com>
2016-03-21 10:44:28 -0700

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -5,5 +5,4 @@ if [[ "$1" == "stack" && "$2" == "install" ]]; then
     rm -rf cinder
     git clone https://github.com/Nexenta/cinder.git
     cp -r cinder/cinder/volume/drivers/nexenta /opt/stack/cinder/cinder/volume/drivers
-    cp cinder/cinder/exception.py /opt/stack/cinder/cinder
 fi
