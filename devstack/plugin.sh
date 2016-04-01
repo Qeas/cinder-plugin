6ab7ffd006b0a8c8c185228252f40adfc16b826c
Qeas <alexey.khodos@nexenta.com>
2016-04-01 16:02:36 -0700

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -4,6 +4,6 @@ if [[ "$1" == "stack" && "$2" == "install" ]]; then
     cd /home/ubuntu
     rm -rf cinder
     git clone https://github.com/Nexenta/cinder.git
-    cp -r cinder/cinder/volume/drivers/nexenta/ns5/iscsi.py /opt/stack/cinder/cinder/volume/drivers/nexenta/ns5
+    cp -r cinder/cinder/volume/drivers/nexenta/ns5/iscsi.py /opt/stack/cinder/cinder/volume/drivers/nexenta/ns5/
     cp -r cinder/cinder/volume/drivers/nexenta/iscsi.py /opt/stack/cinder/cinder/volume/drivers/nexenta/
 fi
