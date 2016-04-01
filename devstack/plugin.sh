3173ef05ce636d0ec56d269eee7e1fcda0ce94a8
Qeas <alexey.khodos@nexenta.com>
2016-04-01 14:29:19 -0700

    ns5 nfs only

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -4,5 +4,5 @@ if [[ "$1" == "stack" && "$2" == "install" ]]; then
     cd /home/ubuntu
     rm -rf cinder
     git clone https://github.com/Nexenta/cinder.git
-    cp -r cinder/cinder/volume/drivers/nexenta /opt/stack/cinder/cinder/volume/drivers
+    cp -r cinder/cinder/volume/drivers/nexenta/ns5/nfs.py /opt/stack/cinder/cinder/volume/drivers/nexenta/ns5
 fi
