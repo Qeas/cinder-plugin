465973543e70d18a72dc1eefdcd55aeb9c47f847
Qeas <alexey.khodos@nexenta.com>
2017-03-17 21:00:14 +0300

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
-    cd /home/ubuntu
+    cd
     rm -rf cinder
     git clone -b $NS_CINDER_BRANCH https://github.com/Nexenta/cinder.git
     cp -rf cinder/cinder/volume/drivers/nexenta/ /opt/stack/cinder/cinder/volume/drivers/
